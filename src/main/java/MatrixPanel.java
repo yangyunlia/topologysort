5da2552fa4983b22c169925a9c7530cc1691c8a1
yyl <yyl1113044380@gmail.com>
2022-02-21 19:51:14 +0800

    feta

--- src/main/java/MatrixPanel.java
+++ src/main/java/MatrixPanel.java
@@ -15,19 +15,20 @@ public class MatrixPanel extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         setBounds(mainFrame.getWidth()/2, mainFrame.getHeight()/10, mainFrame.getWidth()/2, mainFrame.getHeight()*6/10);
         setVisible(true);
-        if (mainFrame.flag == 0) {
-            g2.drawString("邻接矩阵", 30, 10);
-
-        } else {
-            g2.drawString("邻接链表", 30, 10);
-
-        }
         if(mainFrame.graph == null) {
             return;
         }
         int n = mainFrame.graph.getvNum();
         int w=20,h=mainFrame.getHeight()/(n+10);
-        g2.drawString("节点", w, h);
+        if (mainFrame.flag == 0) {
+            g2.drawString("邻接矩阵", w - 10, h);
+
+        } else {
+            g2.drawString("邻接链表", w - 10, h);
+
+        }
+        h += this.getWidth()/(n+10);
+        g2.drawString("节点", w-10, h);
         h += this.getWidth()/(n+10);
         for(int i = 0; i < n; i++) {
             g2.drawString(Integer.toString(i), w, h);
@@ -44,11 +45,14 @@ public class MatrixPanel extends JPanel {
                     if( v != 0) {
                         g2.drawRect(w - 8,h -12 ,20,20);
                         g2.drawString(Integer.toString(j), w, h);
-                        w += this.getWidth()/(n+10);
+                        g2.drawLine(w + 20, h, w + 40, h);
+                        g2.drawLine(w + 40, h, w + 35, h - 5);
+                        g2.drawLine(w + 40, h, w + 35, h + 5);
+                        w += 50;
                     }
                 } else  {
                     g2.drawString(Integer.toString(v), w, h);
-                    w += this.getWidth()/(n+10);
+                    w += 50;
                 }
             }
             h += 50;
